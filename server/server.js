2e69c0f076caab2bf41a4c322b94bba58891d29f
Zhang <shujun_zhang@msn.com>
2018-12-23 14:51:08 -0600

    adding route

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
 const express = require('express');
+
+var app = new express();
+
+const port = 3000;
+
+app.get('', () =>{
+    console.log('this is get');
+})
+
+app.listen(port, () => {
+    console.log(`findfriend listening at port ${port} `);
+})
