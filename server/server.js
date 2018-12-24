b983de7bed8a6f65e95c99c916ea2ccd1296d134
Zhang <shujun_zhang@msn.com>
2018-12-23 19:52:16 -0600

    add mongoose

--- server/server.js
+++ server/server.js
@@ -2,10 +2,18 @@ const express = require('express');
 
 var app = new express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || git 3000;
 
 app.get('', () =>{
     console.log('this is get');
+})
+
+app.post('/setupnewgame', () => {
+    console.log('setting up new game');
+    var game = {
+        date: new Date.toString, 
+        players: []
+    }
 })
 
 app.listen(port, () => {
